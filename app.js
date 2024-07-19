490a4254e877ec980017f8340c233cd4a10b7456
Moath-COE <moathabdelaziz@outlook.com>
2024-07-19 15:37:45 +0300

    Setup playRound function and fix some bugs
    The function plays a round and determine the winner
    and increment the score for him.

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// Variables
+let computerScore = 0;
+let playerScore = 0;
+
 // Functions
 
 function getComputerChoice() {
@@ -31,4 +35,53 @@ function getChoice(x) {
       choice = "Error";
   }
   return choice;
+}
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == "Error" || humanChoice == "Error") {
+    console.log("An error happened, please try again.");
+    return;
+  }
+  console.log("You chose: " + humanChoice);
+  console.log("Computer chose: " + computerChoice);
+  if (humanChoice == "Rock") {
+    if (computerChoice == "Rock") {
+      console.log("Tie, no one wins.");
+      return;
+    } else if (computerChoice == "Paper") {
+      console.log("Computer Wins, Paper beats Rock!");
+      computerScore++;
+      return;
+    } else {
+      console.log("You Win, Rock beats Scissors!");
+      playerScore++;
+      return;
+    }
+  } else if (humanChoice == "Paper") {
+    if (computerChoice == "Paper") {
+      console.log("Tie, no one wins.");
+      return;
+    } else if (computerChoice == "Rock") {
+      console.log("You Win, Paper beats Rock!");
+      playerScore++;
+      return;
+    } else {
+      console.log("Computer Wins, Scissors beats Paper!");
+      computerScore++;
+      return;
+    }
+  } else {
+    if (computerChoice == "Scissors") {
+      console.log("Tie, no one wins.");
+      return;
+    } else if (computerChoice == "Rock") {
+      console.log("Computer Wins, Rock beats Scissors!");
+      computerScore++;
+      return;
+    } else {
+      console.log("You Win, Scissors beats Paper!");
+      playerScore++;
+      return;
+    }
+  }
 }
