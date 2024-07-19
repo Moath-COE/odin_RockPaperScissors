acbb50871e7a6c7fbc512d2968810340c54a1cba
Moath-COE <moathabdelaziz@outlook.com>
2024-07-19 15:09:38 +0300

    Setup getHumanChoice function & getChoice function
    The getHumanChoice function is responsible for
    getting player choice. The getChoice function is
    made to reduce code redundancy.

--- app.js
+++ app.js
@@ -2,8 +2,22 @@
 
 function getComputerChoice() {
   let randomNum = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+  return getChoice(randomNum);
+}
+
+function getHumanChoice() {
+  let choiceNum = parseInt(
+    prompt(`Choose your move: 
+        1.Rock 
+        2.Paper 
+        3.Scissors`)
+  );
+  return getChoice(choiceNum);
+}
+
+function getChoice(x) {
   let choice;
-  switch (randomNum) {
+  switch (x) {
     case 1:
       choice = "Rock";
       break;
